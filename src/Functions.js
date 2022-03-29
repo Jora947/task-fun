416fc8557be074d05c193c1464e8db3005a1bde0
BuildTools <unconfigured@null.spigotmc.org>
2022-03-29 13:38:37 +0500

    Задание по функциям

--- src/Functions.js
+++ src/Functions.js
@@ -1,12 +1,28 @@
 /*
 Напишите коллбек для array.filter, который отбирает все числа больше num
  */
-export function higherThan(num) {}
+export function higherThan(num) {
+    let result = [];
+    for (let i = 0; i < arguments.length; i++) {
+        if (arguments[i] > num) {
+            result.push(arguments[i]);
+        }
+    }
+    return result;
+}
 
 /*
 Напишите коллбек для array.filter, который отбирает из массива все строки, включающие в себя substr. Регистр важен.
  */
-export function hasSubstring(substr) {}
+export function hasSubstring(substr) {
+    let total = [];
+    for (let i = 0; i < arguments.length; i++) {
+        if (arguments[i].includes(substr) === true) {
+            total.push(arguments[i]);
+        }
+    }
+    return total;
+}
 
 /*
 Напишите функцию `multiply(num)`, которая работает вот так:
@@ -14,4 +30,6 @@ export function hasSubstring(substr) {}
 console.log(multiply(5)(10));
 // 50
  */
-export function multiply(num) {}
+export function multiply(num) {
+    return arguments * num;
+}
